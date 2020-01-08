cc7f884adee82ac9fb0bf5f817c210eed92caeda
ansaryergesh <yergeshovansar@gmail.com>
2020-01-09 03:37:59 +0600

    turn , player, message logic started

--- js/app.js
+++ js/app.js
@@ -1,3 +1,25 @@
-function game() {
-  console.log('Im the div inside of  the body');
+//basic variables
+let board;
+let player = [];
+player[0] = "Player1";
+player[1] = "Player2";
+let options = ["X", "O"];
+let turn = 0;
+
+function game(clickBox) {
+  clickBox.innerText = options[turn];
+  switchTurn();
+
+  clickBox.onclick = "";
+  document.getElementById("messages").innerText = player[turn] + "'s turn"
+
+}
+
+function switchTurn() {
+  if(turn == 0) turn = 1;
+  else turn = 0;
+
+
+
+
 }

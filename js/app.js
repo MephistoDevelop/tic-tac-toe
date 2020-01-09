3aed57a4d39b18fc3cb7fffc337453b5e821eba9
Cristian <inesherald@gmail.com>
2020-01-09 12:08:40 -0600

    Finish win, draw and rreset functions

--- js/app.js
+++ js/app.js
@@ -13,8 +13,10 @@ let win = [
   [1, 5, 9],
   [3, 5, 7],
 ];
+let winner = false;
 let options = ['X', 'O'];
 let turn = 0;
+let counter = 0;
 
 function game(clickBox) {
   clickBox.innerText = options[turn];
@@ -23,6 +25,7 @@ function game(clickBox) {
   clickBox.onclick = '';
   document.getElementById('messages').innerText = player[turn] + "'s turn";
   board[clickBox.id] = options[turn];
+  counter++;
   console.log(board);
   winning(board, options[turn]);
 }
@@ -40,24 +43,36 @@ function winning(board, sign) {
       board[element[2] - 1] === sign
     ) {
       alert(`Player ${sign} Wins !! - ${element}`);
+      winner = true;
       resetGame();
     }
     // console.log(element);
   });
+  checkDraw();
 }
 function resetGame() {
   board = {};
   let boxs = document.getElementsByClassName('box');
-  for (let i = 0; i < boxs.length - 1; i++) {
+  for (let i = 0; i < boxs.length; i++) {
     boxs[i].innerText = '';
     boxs[i].addEventListener('click', function() {
       game(this);
     });
     turn = 0;
+    counter = 0;
+    winner = false;
     document.getElementById('messages').innerText = player[turn] + "'s turn";
   }
 }
 function switchTurn() {
-  if (turn == 0) turn = 1;
+  if (turn === 0) turn = 1;
   else turn = 0;
+}
+
+function checkDraw() {
+  console.log(`Yo soy board: ${counter}`);
+  if (counter === 9 && winner === false) {
+    alert('draw game');
+    resetGame();
+  }
 }

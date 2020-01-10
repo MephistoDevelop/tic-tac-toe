7d7710afe366d25413c6450edbc2bb8fe51d7b33
Cristian <inesherald@gmail.com>
2020-01-10 13:10:41 -0600

    Fix some lintern erors

--- js/app.js
+++ js/app.js
@@ -28,13 +28,13 @@ function drawBoard() {
 function giveName() {
   player1 = document.getElementById('player-1');
   player2 = document.getElementById('player-2');
-  if (player1.value == '') {
+  if (player1.value === '') {
     player[0] = 'Player1';
   } else {
     player[0] = player1.value;
   }
 
-  if (player2.value == '') {
+  if (player2.value === '') {
     player[1] = 'Player2';
   } else {
     player[1] = player2.value;
@@ -46,17 +46,29 @@ function giveName() {
   document.getElementById('form').classList.add('hide');
 }
 
+function switchTurn() {
+  if (turn === 0) turn = 1;
+  else turn = 0;
+}
+
+function checkDraw() {
+  if (counter === 9 && winner === false) {
+    document.getElementById('messages').innerText = 'Draw Game';
+    winner = false;
+  }
+}
+
 function game(clickBox) {
   clickBox.innerText = options[turn];
 
-  if (winner != true) {
+  if (winner !== true) {
     switchTurn();
   }
   clickBox.onclick = '';
   clickBox.addEventListener('click', null);
   document.getElementById('messages').innerText = player[turn] + "'s turn";
   board[clickBox.id] = options[turn];
-  counter++;
+  counter += 1;
   winning(board, options[turn]);
 }
 
@@ -74,7 +86,7 @@ function winning(board, sign) {
       document.getElementById('messages').innerText = player[turn] + "'s win";
 
       winner = true;
-      elem = document.getElementsByClassName('box');
+      let elem = document.getElementsByClassName('box');
       for (let i = 0; i < elem.length; i++) {
         elem[i].onclick = '';
       }
@@ -87,15 +99,4 @@ function resetGame() {
   winner = false;
   document.getElementById('messages').innerText = '';
   drawBoard();
-}
-function switchTurn() {
-  if (turn === 0) turn = 1;
-  else turn = 0;
-}
-
-function checkDraw() {
-  if (counter === 9 && winner === false) {
-    document.getElementById('messages').innerText = 'Draw Game';
-    winner = false;
-  }
 }

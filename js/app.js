f0be60bf036d062d7110b1bc102be4162db5d675
ansaryergesh <yergeshovansar@gmail.com>
2020-01-15 14:50:25 +0600

    fixings

--- js/app.js
+++ js/app.js
@@ -17,98 +17,137 @@ const options = ['X', 'O'];
 let turn = 0;
 let counter = 0;
 
-function drawBoard() {
-  const Board = document.getElementById('board');
-  let display = '';
-  for (let i = 1; i < 10; i += 1) {
-    display += `<div id ="${i}" onclick="game(this);" class="box"></div>`;
-  }
-  Board.innerHTML = display;
-}
-function giveName() {
-  const player1 = document.getElementById('player-1');
-  const player2 = document.getElementById('player-2');
-  if (player1.value === '') {
-    player[0] = 'Player1';
-  } else {
-    player[0] = player1.value;
-  }
+class Player {
+    static giveName() {
+        const player1 = document.getElementById('player-1');
+        const player2 = document.getElementById('player-2');
+        if (player1.value === '') {
+          player[0] = 'Player1';
+        } else {
+          player[0] = player1.value;
+        }
+      
+        if (player2.value === '') {
+          player[1] = 'Player2';
+        } else {
+          player[1] = player2.value;
+        }
+        Board.drawBoard();
+      
+        document.getElementById('board').classList.remove('hide');
+        document.getElementById('buttons').classList.remove('hide');
+        document.getElementById('form').classList.add('hide');
+    }
 
-  if (player2.value === '') {
-    player[1] = 'Player2';
-  } else {
-    player[1] = player2.value;
-  }
-  drawBoard();
-
-  document.getElementById('board').classList.remove('hide');
-  document.getElementById('buttons').classList.remove('hide');
-  document.getElementById('form').classList.add('hide');
-}
-
-function switchTurn() {
-  if (turn === 0) turn = 1;
-  else turn = 0;
+    static switchTurn() {
+        if (turn === 0) turn = 1;
+        else turn = 0;
+    }
 }
 
-function checkDraw() {
-  if (counter === 9 && winner === false) {
-    document.getElementById('messages').innerText = 'Draw Game';
-    winner = false;
-    alert('Draw Game !!');
-    resetGame();
-  }
+
+class Board {
+
+    static checkDraw() {
+        if (counter === 9 && winner === false) {
+          document.getElementById('messages').innerText = 'Draw Game';
+          winner = false;
+          alert('Draw Game !!');
+          Game.resetGame();
+        }
+    }
+ 
+    static drawBoard() {
+        const Board = document.getElementById('board');
+        let display = '';
+        for (let i = 1; i < 10; i += 1) {
+          display += `<div id ="${i}" onclick="game(this);" class="box"></div>`;
+        }
+        Board.innerHTML = display;
+    }
+
+    static winning(Board, sign) {
+        const win = [
+            [1, 2, 3],
+            [4, 5, 6],
+            [7, 8, 9],
+            [1, 4, 7],
+            [2, 5, 8],
+            [3, 6, 9],
+            [1, 5, 9],
+            [3, 5, 7],
+        ];
+
+        win.forEach(function(element) {
+          if (
+            board[element[0]] === sign &&
+            board[element[1]] === sign &&
+            board[element[2]] === sign
+          ) {
+            winner = true;
+            const elem = document.getElementsByClassName('box');
+            for (let i = 0; i < elem.length; i += 1) {
+              elem[i].onclick = '';
+            }
+            alert(`Player ${player[turn]} Wins !!`);
+            Game.resetGame();
+          } else {
+            winner = false;
+    
+          }
+        });
+
+        if (counter === 9 && winner === false) {
+            document.getElementById('messages').innerText = 'Draw Game';
+            winner = false;
+            alert('Draw Game !!');
+            Game.resetGame();
+          }
+    }
+
+
+     
 }
+
+class Game {
+    static resetGame() {
+        board = {};
+        winner = false;
+        counter = 0;
+        turn = 1;
+        Board.drawBoard();
+    }
+}
+
 
 function game(clickBox) {
-  clickBox.innerText = options[turn];
-  clickBox.onclick = '';
-  clickBox.addEventListener('click', null);
-
-  board[clickBox.id] = options[turn];
-  counter += 1;
-  winning(board, options[turn]);
-  if (winner !== true) {
-    switchTurn();
-  }
-  document.getElementById('messages').innerText = player[turn] + "'s turn";
-}
+    clickBox.innerText = options[turn];
+    clickBox.onclick = '';
+    clickBox.addEventListener('click', null);
+  
+    board[clickBox.id] = options[turn];
+    counter += 1;
+    Board.winning(board, options[turn]);
+    if (winner !== true) {
+      Player.switchTurn();
+    }
+    document.getElementById('messages').innerText = player[turn] + "'s turn";
+} 
 
 function refresh() {
-  location.reload();
+    location.reload();
 }
 
-function winning(Board, sign) {
-  win.forEach(function(element) {
-    if (
-      board[element[0]] === sign &&
-      board[element[1]] === sign &&
-      board[element[2]] === sign
-    ) {
-      winner = true;
-      const elem = document.getElementsByClassName('box');
-      for (let i = 0; i < elem.length; i += 1) {
-        elem[i].onclick = '';
-      }
-      alert(`Player ${sign} Wins !!`);
-      resetGame();
-    } else {
-      winner = false;
-      checkDraw();
-    }
-  });
-  checkDraw();
-}
 
-function resetGame() {
-  board = {};
-  winner = false;
-  counter = 0;
-  turn = 1;
-  drawBoard();
-}
 
-document.getElementById('btn-play').addEventListener('click', giveName);
-document.getElementById('button1').addEventListener('click', resetGame);
+document.getElementById('btn-play').addEventListener('click', function(){
+    Player.giveName();
+})
+document.getElementById('button1').addEventListener('click', function(){
+    Game.resetGame();
+});
 document.getElementById('button2').addEventListener('click', refresh);
-document.getElementById('body').addEventListener('load', resetGame);
+document.getElementById('body').addEventListener('load', function(){
+    Game.resetGame();
+});
+
